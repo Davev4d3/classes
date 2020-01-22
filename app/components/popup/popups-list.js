a80be00f2f7ae0a611d31a3455d2043a4f07a88c
David Wu <wu.david3283@gmail.com>
2020-01-22 13:04:49 +1100

    remove assessments disclaimer

--- app/components/popup/popups-list.js
+++ app/components/popup/popups-list.js
@@ -1,3 +1,2 @@
 export const PopupsList = [
-  {title: 'Assessments are now shown in \'Today\' and \'Timetable\'. Please note that this does not constitute an official schedule.', instantClose: true},
 ];
